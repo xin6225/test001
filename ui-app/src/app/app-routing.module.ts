27512357ef1ceb716532d2a2d86fca4819c0d215
xin.socher-feng <xin.socher-feng@mapp.com>
2018-02-12 17:30:44 +0100

    init test project UI part

---
+++ ui-app/src/app/app-routing.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+import { EditComponent } from './edit/edit.component';
+import { LoginComponent } from './login/login.component';
+import { AuthGuardService } from './shared/services/auth-guard.service';
+import { AuthService } from './shared/services/auth.service';
+import { OverviewComponent } from './overview/overview.component';
+
+const routes: Routes = [
+  {path: '', redirectTo: '/overview', pathMatch: 'full'},
+  {path: 'login', component: LoginComponent},
+  {path: 'overview', component: OverviewComponent},
+  {path: 'edit/:id', canActivate: [AuthGuardService], component: EditComponent},
+  {path: 'create', canActivate: [AuthGuardService], component: EditComponent}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+  providers: [
+    AuthGuardService,
+    AuthService
+  ]
+})
+
+export class AppRoutingModule {
+}
--- ui-app/src/app/app.module.ts
+++ ui-app/src/app/app.module.ts
@@ -1,18 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientModule } from '@angular/common/http';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 import { AppComponent } from './app.component';
 import { OverviewComponent } from './overview/overview.component';
+import { EditComponent } from './edit/edit.component';
+import { LoginComponent } from './login/login.component';
+import { AppRoutingModule } from './app-routing.module';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    OverviewComponent
+    OverviewComponent,
+    EditComponent,
+    LoginComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
+    AppRoutingModule,
+    MatToolbarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
